bba7e1b89b44a43c2160ca37c87e485322267c50
Joshua de Lange <joshua.delange@gmail.com>
2014-05-29 20:41:13 +0200

    adds reporting function! :D

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,31 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def user_edit
-
-  	
-
   end
+
+  def reports
+
+    where = ''
+
+    @start_date = params[:start_date]
+    if @start_date.blank?
+      @start_date = Date.today
+    end
+
+    @end_date = params[:end_date]
+    if @end_date.blank?
+      @end_date = Date.today + 30
+    end
+
+    unless @start_date.blank?
+      where = "day >= '#{@start_date}'"
+    end
+
+    unless @end_date.blank?
+      where = "day <= '#{@end_date}'"
+    end
+
+    @signups = Signup.select('name, meal, count(*) as times').where(where).group('name, meal').order('name')
+  end
+
 end
